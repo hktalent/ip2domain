7c4665c7c14c11e1b9801b7f8be76cf72db1f9b3
51pwn MTX <hktalent@qq.com>
2021-10-21 15:14:11 +0800

    up

--- update.sh
+++ update.sh
@@ -1,4 +1,10 @@
+rm -rf ip2d.txt
+scp -i ~/.ssh/id_rsa -r -P $myVpsPort root@51pwn.com:/root/ip2d.txt ./
+sort -u ip2d.txt|uniq >ip2domain.txt
+rm -rf ip2d.txt
+mysqlimport -u sgdb_51pwn -psgdb_51pwn --local sgdb_51pwn  `pwd`/ip2domain.txt
 rm -rf ip2domain.txt
+
 mysqldump  --set-gtid-purged=OFF --host=127.0.0.1 --protocol=tcp -C  --user=sgdb_51pwn --lock-tables=FALSE --port=3306 --default-character-set=utf8  "sgdb_51pwn" "ip2domain" --password='sgdb_51pwn'> init_data.sql
 git commit -m "up" .
 git push
